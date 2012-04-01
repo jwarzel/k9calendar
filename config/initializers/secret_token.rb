6c21585b8db98af97ef0b935a58b98978b051394
Stephen Anderson and Judy Warzel <dev+bendycode+jwarzel@bendyworks.com>
2012-04-01 11:30:38 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+K9calendar::Application.config.secret_token = '9b746495ef05ade07cf293df6d310d7782d034d4fd3d4c8e91487e08d7200dd806e9d6e7ca1157c2afd15a1241b0eee9867a261b5a668cf24cc0067f2b14f615'
